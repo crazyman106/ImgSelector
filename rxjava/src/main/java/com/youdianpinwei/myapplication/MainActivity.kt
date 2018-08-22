bbb180cec0513dd6f028455775d5c25ef909b646
李俊杰 <291924028@qq.com>
2018-08-22 18:23:54 +0800

    canvas开始阅读源码

--- rxjava/src/main/java/com/youdianpinwei/myapplication/MainActivity.kt
+++ rxjava/src/main/java/com/youdianpinwei/myapplication/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.youdianpinwei.myapplication
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import io.reactivex.Observable
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
